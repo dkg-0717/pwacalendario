5689e87657ef9c70886ec383d42808ef29d8ffa4
Angel <dkg.0717@gmail.com>
2019-03-24 20:56:26 -0600

    deploy

---
+++ precache-manifest.ba04256172a3e431187946be061bafef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/pwacalendario/robots.txt"
+  },
+  {
+    "revision": "213880790f916fcc73ba",
+    "url": "/pwacalendario/js/chunk-vendors.bcfd2efe.js"
+  },
+  {
+    "revision": "b180adf7c452933a9a87",
+    "url": "/pwacalendario/js/app.3fe369ce.js"
+  },
+  {
+    "revision": "9feee9624093a8221a897a40ee86c072",
+    "url": "/pwacalendario/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwacalendario/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "213880790f916fcc73ba",
+    "url": "/pwacalendario/css/chunk-vendors.ed1b120b.css"
+  },
+  {
+    "revision": "b180adf7c452933a9a87",
+    "url": "/pwacalendario/css/app.129307d9.css"
+  }
+];
